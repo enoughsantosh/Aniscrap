c8d7463217396e485171a944e79a4e27e7b61fa2
ENOUGH BRO <kalu4134@gmail.com>
2025-02-22 21:46:39 +0530

    Update and rename main.py to api/main.py

--- api/main.py
+++ api/main.py
@@ -1,8 +1,17 @@
 from fastapi import FastAPI
 import requests
 from bs4 import BeautifulSoup
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
+
+# Enable CORS for frontend to access backend
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 @app.get("/anime")
 def get_anime():
